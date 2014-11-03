aa64d1b53c31ae69a2558b69c3e159ad0996d6cd
zxfhacker <zxfhacker@gmail.com>
2014-11-03 18:23:07 +0800

    添加App检查更新的部分通用代码

--- src/com/alex/develop/BaseActivity.java
+++ src/com/alex/develop/BaseActivity.java
@@ -1,6 +1,10 @@
 package com.alex.develop;
 
 import com.alex.develop.util.WindowHelper;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -17,6 +21,60 @@ public class BaseActivity extends FragmentActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		WindowHelper.initialize(this);
+		
+		checkForUpdate();
+	}
+	
+	/**
+	 * 检测更新
+	 */
+	private void checkForUpdate() {
+		
+		// TODO 联网操作取得更新信息
+		int remoteVersionCode = 2;
+		String remoteVersionName = "0.1.2";
+		String remoteAppUrl = "";
+		
+		int localVersionCode = getAppVersionCode();
+		String localVersionName = getAppVersionName();
+		
+		if(localVersionCode < remoteVersionCode) {
+			
+		}
+	}
+
+	/**
+	 * 获取App的版本名称
+	 * @return App版本名称 
+	 */
+	public String getAppVersionName() {
+		String versionName = null;
+		try {
+			PackageManager pm = getPackageManager();
+			PackageInfo pInfo = pm.getPackageInfo(getPackageName(), 0);
+			versionName = pInfo.versionName;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return versionName;
+	}
+	
+	/**
+	 * 获取App的版本号
+	 * @return App版本号
+	 */
+	public int getAppVersionCode() {
+		int versionCode = -1;
+		try {
+			PackageManager pm = getPackageManager();
+			PackageInfo pInfo = pm.getPackageInfo(getPackageName(), 0);
+			versionCode = pInfo.versionCode;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return versionCode;
 	}
 }
